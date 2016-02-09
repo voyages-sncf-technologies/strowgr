be93110fc480d641a2d9e00e3adbabf48f99bd28
Thomas Auffredou <tauffredou@xebia.fr>
2016-02-09 18:09:39 +0100

    Update webapp Makefile

--- haaasd/clean-nsq.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-LOOKUPD=floradora:50161
-
-curl -X POST http://$LOOKUPD/topic/delete?topic=try_update_default-name
-curl -X POST http://$LOOKUPD/topic/delete?topic=update_default-name
-curl -X POST http://$LOOKUPD/topic/delete?topic=updated_default-name
--- webapp/build.Dockerfile
+++ webapp/build.Dockerfile
@@ -1,6 +1,6 @@
 FROM golang:1.5
 
-ENV SRC /go/src/gitlab.socrate.vsct.fr/dt/haaas/web
+ENV SRC /go/src/gitlab.socrate.vsct.fr/dt/haaas
 
 RUN mkdir -p $SRC
 WORKDIR $SRC
