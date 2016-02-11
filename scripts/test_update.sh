87d43ffc33c47981439d40f78c0e2258d3d5d926
Thomas Auffredou <tauffredou@xebia.fr>
2016-02-11 16:45:56 +0100

    Add test script

--- scripts/test_update.sh
+++ scripts/test_update.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
+set -x
 CWD=$(cd $(dirname $0);pwd)
 TS=$(date +%s)
-base64 -i "$CWD/../data/HAsample.conf"
-BASE64=$(base64 -i "$CWD/data/../HAsample.conf" )
-DATA="{'conf' : '$BASE64','timestamp' : $TS, 'correlationid' : '$UUID', 'application' : 'OCE', 'platform' : 'REC1' }"
-echo $D
+BASE64=$(base64 -i "$CWD/../data/HAsample.conf" )
+DOCKER_HOST=$(docker-machine ip default)
+UUID=$(uuidgen)
+DATA=$(cat <<- OEF
+{
+    "conf" : "$BASE64",
+    "timestamp" : $TS,
+    "correlationid" : "$UUID",
+    "application" : "OCE",
+    "platform" : "REC1"
+}
+EOF)
+
+curl -d "$DATA" -H "Content-Type: application/json" -X POST http://$DOCKER_HOST:4151/pub?topic=try_update_default-name
