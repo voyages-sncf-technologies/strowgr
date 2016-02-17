3ee36916adfab14a6b926fdd01a43a7b0a7adbbe
William Montaz <wmontaz.prestataire@voyages-sncf.com>
2016-02-17 14:17:18 +0100

    CorrelationID provided as UUID

--- admin-core/src/main/java/com/vsct/dt/haas/admin/core/event/CorrelationId.java
+++ admin-core/src/main/java/com/vsct/dt/haas/admin/core/event/CorrelationId.java
@@ -1,16 +1,15 @@
 package com.vsct.dt.haas.admin.core.event;
 
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * Created by william_montaz on 10/02/2016.
  */
 public class CorrelationId {
 
-    private static Random random = new Random();
-
     public static String newCorrelationId() {
-        return Long.toString(random.nextLong());
+        return UUID.randomUUID().toString();
     }
 
 }
