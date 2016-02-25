fb3d276fa2c794c793a17f8b8ea0b4ff757138f5
Thomas Auffredou <tauffredou@xebia.fr>
2016-02-25 21:07:54 +0100

    Add ab stress test

---
+++ scripts/stress.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+ab -r -c 20 -n 100000 -g stress-hap.tsv http://$(docker-machine ip default):52000/
+# Let's output to a jpeg file
+set terminal jpeg size 500,500
+# This sets the aspect ratio of the graph
+set size 1, 1
+# The file we'll write to
+set output "graphs/stress-hap.jpg"
+# The graph title
+set title "Benchmark testing"
+# Where to place the legend/key
+set key left top
+# Draw gridlines oriented on the y axis
+set grid y
+# Specify that the x-series data is time data
+set xdata time
+# Specify the *input* format of the time data
+set timefmt "%s"
+# Specify the *output* format for the x-axis tick labels
+set format x "%S"
+# Label the x-axis
+set xlabel 'seconds'
+# Label the y-axis
+set ylabel "response time (ms)"
+# Tell gnuplot to use tabs as the delimiter instead of spaces (default)
+set datafile separator '\t'
+# Plot the data
+PLOT=$(which plot && echo "plot" || echo "gnuplot")
+$PLOT "stress-hap.tsv" every ::2 using 2:5 title 'response time' with points
+exit
