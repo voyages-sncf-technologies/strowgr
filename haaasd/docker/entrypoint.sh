f47e82869d7becf8b3d1d4b94f09f0ca6d98232a
Auffredou Thomas <tauffredou.prestataire@voyages-sncf.com>
2016-03-02 16:03:34 +0100

    Allow 10 platforms

--- haaasd/docker/entrypoint.sh
+++ haaasd/docker/entrypoint.sh
@@ -3,7 +3,7 @@
 # Create users
 for APP in OCE ECE MPD PAO;do
     for PLT in r u e p h;do
-        for N in $(seq 1 2);do
+        for N in $(seq 1 10);do
             useradd $(echo hap$APP$PLT$N | tr '[:upper:]' '[:lower:]')
         done
     done
