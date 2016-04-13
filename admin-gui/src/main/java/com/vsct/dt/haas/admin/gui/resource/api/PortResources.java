df11bd0f0f61aa69d39865138ba1018fc139472b
WMO <wmontaz@voyages-sncf.com>
2016-04-13 14:36:04 +0200

    Refacto Portresources Rest style

--- admin-gui/src/main/java/com/vsct/dt/haas/admin/gui/resource/api/PortResources.java
+++ admin-gui/src/main/java/com/vsct/dt/haas/admin/gui/resource/api/PortResources.java
@@ -3,13 +3,26 @@ package com.vsct.dt.haas.admin.gui.resource.api;
 import com.vsct.dt.haas.admin.core.PortProvider;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.HashMap;
 import java.util.Map;
 
 @Path("/ports")
 public class PortResources {
 
-    @POST
+    private final PortProvider portProvider;
+
+    public PortResources(PortProvider portProvider) {
+        this.portProvider = portProvider;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Integer> getPorts() {
+        return portProvider.getPorts().orElseGet(HashMap::new);
+    }
+
+    @PUT
     @Path("/{id : .+}")
     public String setPort(@PathParam("id") String id) {
         return String.valueOf(portProvider.newPort(id));
@@ -22,18 +35,5 @@ public class PortResources {
                 .map(String::valueOf)
                 .orElseThrow(NotFoundException::new);
     }
-
-    @GET
-    @Path("/list")
-    public Map<String, Integer> getPorts() {
-        return portProvider.getPorts().orElseGet(HashMap::new);
-    }
-
-    private final PortProvider portProvider;
-
-    public PortResources(PortProvider portProvider) {
-        this.portProvider = portProvider;
-    }
-
 
 }
