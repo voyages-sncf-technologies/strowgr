864866b73a2fc4b02b1cc924b13a68e29b2cb0dc
William Montaz <wmontaz@voyages-sncf.com>
2016-04-29 14:07:39 +0200

    Fix bug on multimap implementation. Improve logs

--- event-gui/src/main/java/com/vsct/haas/monitoring/gui/cassandra/CassandraClient.java
+++ event-gui/src/main/java/com/vsct/haas/monitoring/gui/cassandra/CassandraClient.java
@@ -1,18 +1,14 @@
 package com.vsct.haas.monitoring.gui.cassandra;
 
 import com.datastax.driver.core.*;
-import com.google.common.collect.Multiset;
-import com.google.common.collect.SortedSetMultimap;
-import com.google.common.collect.TreeMultimap;
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
 import com.google.common.util.concurrent.AsyncFunction;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -21,13 +17,12 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CassandraClient {
 
     static class Value {
-        Date timestamp;
+        Date   timestamp;
         String name;
         String payload;
     }
 
     static class Key {
-        Date timestamp;
         String correlationId;
 
         @Override
@@ -49,18 +44,7 @@ public class CassandraClient {
         }
     }
 
-
-    static SortedSetMultimap<Key, Value> result = TreeMultimap.create(new Comparator<Key>() {
-        @Override
-        public int compare(Key o1, Key o2) {
-            return o1.timestamp.compareTo(o2.timestamp);
-        }
-    }, new Comparator<Value>() {
-        @Override
-        public int compare(Value o1, Value o2) {
-            return o1.timestamp.compareTo(o2.timestamp);
-        }
-    });
+    static LinkedHashMultimap<Key, Value> result = LinkedHashMultimap.create();
 
     static AtomicInteger messageProcessed = new AtomicInteger(0);
     static Boolean showPayload;
@@ -75,14 +59,14 @@ public class CassandraClient {
         showPayload = Boolean.valueOf(System.getenv("showPayload"));
 
         Date sinceDate = null;
-        if(since != null && since.contains("s"))
-            sinceDate = Date.from(LocalDateTime.now().minusSeconds(Long.valueOf(since.replace("s",""))).atZone(ZoneId.systemDefault()).toInstant());
-        if(since != null && since.contains("m"))
+        if (since != null && since.contains("s"))
+            sinceDate = Date.from(LocalDateTime.now().minusSeconds(Long.valueOf(since.replace("s", ""))).atZone(ZoneId.systemDefault()).toInstant());
+        if (since != null && since.contains("m"))
             sinceDate = Date.from(LocalDateTime.now().minusMinutes(Long.valueOf(since.replace("m", ""))).atZone(ZoneId.systemDefault()).toInstant());
-        if(since != null && since.contains("h"))
+        if (since != null && since.contains("h"))
             sinceDate = Date.from(LocalDateTime.now().minusHours(Long.valueOf(since.replace("h", ""))).atZone(ZoneId.systemDefault()).toInstant());
 
-        if(node == null | keyspace == null | entrypoint == null | date == null | since == null | showPayload == null | sinceDate == null){
+        if (node == null | keyspace == null | entrypoint == null | date == null | since == null | showPayload == null | sinceDate == null) {
             System.out.println("You must provide these environment variables :");
             System.out.println("\t- cassandra.node");
             System.out.println("\t- cassandra.keyspace");
@@ -93,7 +77,8 @@ public class CassandraClient {
             return;
         }
 
-
+        System.out.println("Initiating connection with Cassandra on "+node);
+        
         Cluster cluster = Cluster.builder()
                 .addContactPoint(node)
                 .withQueryOptions(new QueryOptions().setFetchSize(500))
@@ -102,45 +87,45 @@ public class CassandraClient {
         Session session = cluster.connect(keyspace);
 
         String query;
-        if(showPayload) {
+        if (showPayload) {
             query = "select event_timestamp,event_name,correlation_id,payload from entrypoint_by_day where id='" + entrypoint + "' and date='" + date + "' and event_timestamp > " + sinceDate.getTime() + ";";
-        } else {
+        }
+        else {
             query = "select event_timestamp,event_name,correlation_id from entrypoint_by_day where id='" + entrypoint + "' and date='" + date + "' and event_timestamp > " + sinceDate.getTime() + ";";
         }
 
-        System.out.println("Cassandra query="+query);
+        System.out.println("Cassandra query=" + query+"\n");
 
         Statement statement = new SimpleStatement(query);
 
         ResultSetFuture resultSetFuture = session.executeAsync(statement);
-        ListenableFuture<SortedSetMultimap<Key, Value>> futur = Futures.transform(
+        ListenableFuture<Multimap<Key, Value>> futur = Futures.transform(
                 resultSetFuture,
                 iterate(result)
         );
 
-        futur.get().keys().forEach(k -> {
+        futur.get().keySet().forEach(k -> {
             System.out.print(k.correlationId + " ");
             result.get(k).forEach(v -> {
                 if (showPayload) {
-                    System.out.print(v.name + "|"+v.payload+"| ->");
+                    System.out.print(v.name +"("+v.timestamp+")|" + v.payload + "| ->");
                 }
                 else {
-                    System.out.print(v.name + " -> ");
+                    System.out.print(v.name +"("+v.timestamp+") -> ");
                 }
             });
-            System.out.println();
+            System.out.println("END");
         });
 
+        System.out.println("\nClosing Cassandra connection");
         session.close();
         cluster.close();
-
-        System.out.println("Processed "+messageProcessed+" messages. Map contains "+result.size()+" keys.");
     }
 
-    private static AsyncFunction<ResultSet, SortedSetMultimap<Key, Value>> iterate(SortedSetMultimap<Key, Value> result) {
-        return new AsyncFunction<ResultSet, SortedSetMultimap<Key, Value>>() {
+    private static AsyncFunction<ResultSet, Multimap<Key, Value>> iterate(Multimap<Key, Value> result) {
+        return new AsyncFunction<ResultSet, Multimap<Key, Value>>() {
             @Override
-            public ListenableFuture<SortedSetMultimap<Key, Value>> apply(ResultSet rs) throws Exception {
+            public ListenableFuture<Multimap<Key, Value>> apply(ResultSet rs) throws Exception {
                 int remaining = rs.getAvailableWithoutFetching();
 
                 for (Row row : rs) {
@@ -148,23 +133,28 @@ public class CassandraClient {
                     String name = row.get(1, String.class);
                     String correlationId = row.get(2, UUID.class).toString();
 
-                    Key k = new Key();k.timestamp=d;k.correlationId=correlationId;
-                    Value v = new Value();v.name=name;v.timestamp=d;
+                    Key k = new Key();
+                    k.correlationId = correlationId;
+                    Value v = new Value();
+                    v.name = name;
+                    v.timestamp = d;
 
-                    if(showPayload){
+                    if (showPayload) {
                         v.payload = row.get(3, String.class);
                     }
 
-                    result.put(k,v);
+                    result.put(k, v);
+
                     messageProcessed.incrementAndGet();
-                    if(--remaining == 0)
+                    if (--remaining == 0)
                         break;
                 }
 
                 boolean wasLastPage = rs.getExecutionInfo().getPagingState() == null;
-                if(wasLastPage){
+                if (wasLastPage) {
                     return Futures.immediateFuture(result);
-                } else {
+                }
+                else {
                     ListenableFuture<ResultSet> future = rs.fetchMoreResults();
                     return Futures.transform(future, iterate(result));
                 }
