51fe4782e34fea3e78087b96ab5fd8bb2355b5d2
garnaud <garnaud25@gmail.com>
2016-06-01 19:34:37 +0200

    expose a delete entrypoint service

--- admin/admin-core/src/main/java/com/vsct/dt/strowgr/admin/core/EntryPointRepository.java
+++ admin/admin-core/src/main/java/com/vsct/dt/strowgr/admin/core/EntryPointRepository.java
@@ -2,6 +2,7 @@ package com.vsct.dt.strowgr.admin.core;
 
 import com.vsct.dt.strowgr.admin.core.configuration.EntryPoint;
 
+import java.io.IOException;
 import java.util.Optional;
 import java.util.Set;
 
@@ -22,13 +23,22 @@ public interface EntryPointRepository {
 
     /**
      * Sets the committing configuration with a TTL
-     * @param key of the entrypoint
+     *
+     * @param key           of the entrypoint
      * @param configuration conent of the entrypoint
-     * @param ttl the ttl in seconds
+     * @param ttl           the ttl in seconds
      */
     void setCommittingConfiguration(String correlationId, EntryPointKey key, EntryPoint configuration, int ttl);
 
     void removeCommittingConfiguration(EntryPointKey key);
+
+    /**
+     * Remove this entrypoint from Strowgr. This command must be forward to all Strowgr components (sidekick, database...)
+     *
+     * @param entryPointKey of the entrypoint to delete (ex. MY_APP/PROD1)
+     * @return {@link Boolean#TRUE} if entrypoint has been removed from repository, {@link Boolean#FALSE} if entrypoint has not been found, null otherwise
+     */
+    Boolean removeEntrypoint(EntryPointKey entryPointKey);
 
     void setCurrentConfiguration(EntryPointKey key, EntryPoint configuration);
 
