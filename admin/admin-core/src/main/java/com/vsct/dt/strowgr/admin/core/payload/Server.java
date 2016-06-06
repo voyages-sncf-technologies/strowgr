5f29f85bda2e6f07553f7c791f7b91604048e997
Auffredou Thomas <tauffredou.prestataire@voyages-sncf.com>
2016-06-06 14:30:15 +0200

    [admin] Add new payloads

---
+++ admin/admin-core/src/main/java/com/vsct/dt/strowgr/admin/core/payload/Server.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2016 VSCT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.vsct.dt.strowgr.admin.core.payload;
+
+import java.util.Map;
+
+public class Server {
+    private String id;
+    private String backendId;
+    private String ip;
+    private int port;
+    private Map<String,String> context;
+
+    public Server() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBackendId() {
+        return backendId;
+    }
+
+    public void setBackendId(String backendId) {
+        this.backendId = backendId;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public Map<String, String> getContext() {
+        return context;
+    }
+
+    public void setContext(Map<String, String> context) {
+        this.context = context;
+    }
+}
