2b7847c05232d324d40772fdaab90abbead6558e
Guillaume Arnaud <garnaud25@gmail.com>
2016-10-04 00:38:51 +0200

    [tests] fix unit tests

--- admin/admin-nsq/src/main/java/com/vsct/dt/strowgr/admin/nsq/payload/fragment/Conf.java
+++ admin/admin-nsq/src/main/java/com/vsct/dt/strowgr/admin/nsq/payload/fragment/Conf.java
@@ -39,12 +39,6 @@ public class Conf {
     @JsonProperty("syslog")
     private String syslog;
 
-    /**
-     * Haproxy.
-     */
-    @JsonProperty("haproxyVersion")
-    private String haproxyVersion;
-
     @JsonCreator
     public Conf() {
     }
