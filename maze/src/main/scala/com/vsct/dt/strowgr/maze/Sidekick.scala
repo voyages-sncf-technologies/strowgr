f93156e27329fda6b7a1537e0a0700d4b9299eae
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-01-23 18:06:06 +0100

    depend on sidekick image in docker hub rather than building it again

--- maze/src/main/scala/com/vsct/dt/strowgr/maze/Sidekick.scala
+++ maze/src/main/scala/com/vsct/dt/strowgr/maze/Sidekick.scala
@@ -9,9 +9,9 @@ import fr.vsct.dt.maze.core.Predef._
 object  Sidekick {
 
   class SidekickNode(lookup: String, nsqHost: String, mono: Boolean = false) extends SingleContainerClusterNode with HttpEnabled{
-    override def serviceContainer: CreateContainerCmd = "strowgr/sidekick:0.2.4"
+    override def serviceContainer: CreateContainerCmd = "strowgr/sidekick:0.2.6"
       .withCmd("-verbose=true")
-      .withEnv(s"LOOKUP_ADDR=$lookup:4161", s"PRODUCER_ADDR=$nsqHost:4150", s"PRODUCER_REST_ADDR=http://$nsqHost:4151", s"VIP=$hostname", s"ID=$hostname")
+      .withEnv(s"LOOKUP_ADDR=$lookup:4161", s"PRODUCER_ADDR=$nsqHost:4150", s"PRODUCER_REST_ADDR=http://$nsqHost:4151", "CLUSTER_ID=preproduction", s"VIP=$hostname", s"ID=$hostname")
     override def servicePort: Int = 50000
 
     def isReady(): Predicate = {
