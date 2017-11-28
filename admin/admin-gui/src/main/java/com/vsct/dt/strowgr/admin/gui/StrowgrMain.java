81675b5c1b6c4b3b577c7e05e820f2dabf470d69
Guillaume Arnaud <garnaud@voyages-sncf.com>
2017-11-28 17:48:37 +0100

    improve docker-compose stack for local testing

--- admin/admin-gui/src/main/java/com/vsct/dt/strowgr/admin/gui/StrowgrMain.java
+++ admin/admin-gui/src/main/java/com/vsct/dt/strowgr/admin/gui/StrowgrMain.java
@@ -59,14 +59,18 @@ import io.reactivex.processors.PublishProcessor;
 import io.reactivex.processors.UnicastProcessor;
 import io.reactivex.schedulers.Schedulers;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import java.util.EnumSet;
 import java.util.concurrent.TimeUnit;
 
 public class StrowgrMain extends Application<StrowgrConfiguration> {
@@ -196,6 +200,18 @@ public class StrowgrMain extends Application<StrowgrConfiguration> {
 
         /* Exception mappers */
         environment.jersey().register(new IncompleteConfigurationExceptionMapper());
+
+        /* disabling CORS */
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+
+        // Configure CORS parameters
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
     }
 
     private Publisher<HAProxyPublisher.HAProxyAction> haProxyActionPublisher(StrowgrConfiguration configuration, Environment environment, ConsulRepository repository) {
